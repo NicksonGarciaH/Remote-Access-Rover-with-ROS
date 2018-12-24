f80c4e24e4406aa0e3d93d352160f05fb49afca0
Nickson Garcia <nick.garcia0906@gmail.com>
2018-12-23 22:01:05 -0500

    Update rover_config.py

--- rover_config.py
+++ rover_config.py
@@ -6,21 +6,21 @@ time.sleep(0.2)
 
 os.system("echo 1 > /sys/class/gpio/gpio57/value")      #GPIO 57 HIGH
 print "GPIO 57 ON"
-time.sleep(0.5)
+time.sleep(0.2)
 os.system("echo 0 > /sys/class/gpio/gpio57/value")      #GPIO 57 LOW
 print "GPIO 57 OFF"
-time.sleep(0.5)
+time.sleep(0.2)
 
 os.system("echo 1 > /sys/class/gpio/gpio57/value")
 print "GPIO 57 ON"
-time.sleep(0.5)
+time.sleep(0.2)
 os.system("echo 0 > /sys/class/gpio/gpio57/value")
 print "GPIO 57 OFF"
-time.sleep(0.5)
+time.sleep(0.2)
 
 os.system("echo 1 > /sys/class/gpio/gpio57/value")
 print "GPIO 57 ON"
-time.sleep(0.5)
+time.sleep(0.2)
 os.system("echo 0 > /sys/class/gpio/gpio57/value")
 print "GPIO 57 OFF"
 
