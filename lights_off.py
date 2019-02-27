d483500203bccc460a2846187fc9f756fa9f2211
Nickson Garcia <nick.garcia0906@gmail.com>
2019-02-27 22:04:42 +0000

    Add files via upload

--- lights_off.py
+++ lights_off.py
@@ -1,12 +1,9 @@
 import time, os
 
-os.system("echo 165 > /sys/class/gpio/export") #Enable GPIO 165 = PIN 55 = LEDS
-os.system("echo out > /sys/class/gpio/gpio165/direction")        #GPIO 165 OUT
-
-os.system("echo 166 > /sys/class/gpio/export") #Enable GPIO 166 = PIN 58 = LASER
-os.system("echo out > /sys/class/gpio/gpio166/direction")        #GPIO 166 OUT
+os.system("echo 57 > /sys/class/gpio/export")   #Enable GPIO 57
+os.system("echo out > /sys/class/gpio/gpio57/direction")        #GPIO 57 OUT
 time.sleep(0.1)
 
-os.system("echo 0 > /sys/class/gpio/gpio165/value")      #GPIO 165 LOW
-os.system("echo 0 > /sys/class/gpio/gpio166/value")      #GPIO 166 LOW
+os.system("echo 0 > /sys/class/gpio/gpio57/value")      #GPIO 57 LOW
 print "LIGHTS OFF"
+
