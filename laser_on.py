45b91ae2b8e3471e657ce0d9f091963b3c3e79e4
Nickson Garcia <nick.garcia0906@gmail.com>
2019-02-27 22:07:52 +0000

    Update laser_on.py

--- laser_on.py
+++ laser_on.py
@@ -1,8 +1,4 @@
 import time, os
 
-os.system("echo 166 > /sys/class/gpio/export")   #Enable GPIO 166 = PIN 58
-os.system("echo out > /sys/class/gpio/gpio166/direction")        #GPIO 166 OUT
-time.sleep(0.1)
-
 os.system("echo 1 > /sys/class/gpio/gpio166/value")      #GPIO 166 HIGH
 print "LASER ON"
