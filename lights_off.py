43734cb55a8b4c7c82fcbdd33c90eaef97abb2b8
Nickson Garcia <nick.garcia0906@gmail.com>
2019-02-27 22:09:52 +0000

    Update lights_off.py

--- lights_off.py
+++ lights_off.py
@@ -1,9 +1,7 @@
 import time, os
 
-os.system("echo 57 > /sys/class/gpio/export")   #Enable GPIO 57
-os.system("echo out > /sys/class/gpio/gpio57/direction")        #GPIO 57 OUT
-time.sleep(0.1)
-
-os.system("echo 0 > /sys/class/gpio/gpio57/value")      #GPIO 57 LOW
+os.system("echo 0 > /sys/class/gpio/gpio165/value")      #GPIO 165 LOW
+os.system("echo 0 > /sys/class/gpio/gpio166/value")      #GPIO 166 LOW
 print "LIGHTS OFF"
+
 
